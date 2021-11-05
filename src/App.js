ff7b8d78f6118b669d56cc859ba83b13e3c0eea1
Carlos Hernández <juancarlos.hernandez@gqc.mx>
2021-11-04 21:59:07 -0600

    Initialize using create-react-app and implement react beautiful dnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import './App.css';
 
+const initialTasks = [
+  { id: "1", text: "React.js" },
+  { id: "2", text: "Angular" },
+  { id: "3", text: "Electron" },
+  { id: "4", text: "Graphql" }
+];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext
+      onDragEnd={(result) => {
+        const { source, destination } = result;
+        if (!destination) {
+          return;
+        }
+        if (
+          source.index === destination.index &&
+          source.droppableId === destination.droppableId
+        ) {
+          return;
+        }
+
+        setTasks((prevTasks) =>
+          reorder(prevTasks, source.index, destination.index)
+        );
+      }}
+    >
+      <div className="app">
+        <h1>Estudiar</h1>
+        <Droppable droppableId="tasks">
+          {(droppableProvided) => (
+            <ul
+              {...droppableProvided.droppableProps}
+              ref={droppableProvided.innerRef}
+              className="task-container"
+            >
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(draggableProvided) => (
+                    <li
+                      {...draggableProvided.draggableProps}
+                      ref={draggableProvided.innerRef}
+                      {...draggableProvided.dragHandleProps}
+                      className="task-item"
+                    >
+                      {task.text}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {droppableProvided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
